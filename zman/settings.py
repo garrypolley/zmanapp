eef333fb48df4ab75060d2ffc37676a0cd4c2ba8
Garry Polley <garrympolley@gmail.com>
2012-07-05 20:23:31 -0500

    fixing the settings to work out

--- zman/settings.py
+++ zman/settings.py
@@ -165,6 +165,9 @@ SOCIAL_AUTH_PIPELINE = (
     'social_auth.backends.pipeline.misc.save_status_to_session',
 )
 
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
